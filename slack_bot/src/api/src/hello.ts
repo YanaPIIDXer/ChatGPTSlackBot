dda1b14a9358ba3a9f6669d2f41487efe1bae0c0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-14 03:53:13 +0900

    helloの返答内容をChatGPT経由で返すようにした

--- slack_bot/src/api/src/hello.ts
+++ slack_bot/src/api/src/hello.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from "express";
 import { say } from "../../modules/slack";
 
+// TODO: 別モジュールに移動する
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_TOKEN,
+});
+const openAiApi = new OpenAIApi(configuration);
+
 /**
  * 挨拶API
  * 疎通確認用
  */
 export const helloHanlder = async (req: Request, res: Response) => {
-  try { 
-    const msg = process.env.HELLO_MESSAGE ?? "INVALID ENVIRONMENT VARIABLE";
-    await say("test", msg);
+  try {
+    const prompt = process.env.HELLO_MESSAGE ?? "環境変数が設定されていませんでした。";
+    const gptResponse = await openAiApi.createChatCompletion({
+      model: "gpt-3.5-turbo-0301",
+      messages: [{ role: "user", content: prompt }]
+    });
+    
+    await say("bot実験コーナー", gptResponse.data.choices[0].message?.content || "INVALID RESPONSE");
 
     res.status(200).json({
       body: {
