0fdce742ecd346456e75d5a45561a5fc7b9341d6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-13 21:48:45 +0900

    ひとまずローカルでのレスポンス確認

--- slack_bot/src/index.ts
+++ slack_bot/src/index.ts
@@ -3,11 +3,11 @@ import { APIGatewayProxyResult } from "aws-lambda";
 /**
  * EntryPoint
  */
-exports.handler = (): APIGatewayProxyResult => {
+exports.handler = async (): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Hello, World",
-    })
+    }),
   };
 };
