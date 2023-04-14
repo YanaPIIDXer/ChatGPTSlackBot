5f31ec1b3fefd6b2a86075f86d9581d9ae9ced44
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-15 02:23:09 +0900

    リクエストとレスポンスをログに吐き出すようにした

--- slack_bot/src/index.ts
+++ slack_bot/src/index.ts
@@ -5,6 +5,15 @@ import * as apiHandlers from "./api";
 const app = express();
 app.use(express.json());
 
+// リクエスト・レスポンスログ出力
+app.use((req: Request, res: Response, next: Function) => {
+  console.log("RECV REQUEST", req.body);
+  res.on("finish", () => {
+    console.log("SEND RESPONSE", `${req.method} ${req.url} ${res.statusCode} ${res.statusMessage}`);
+  });
+  next();
+});
+
 // APIハンドラ定義
 app.get("/hello", apiHandlers.helloHandler);
 app.post("/echo", apiHandlers.echoHandler);
